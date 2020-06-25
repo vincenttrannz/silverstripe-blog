7152a0e4b442d1351b7d34afa175f09d73d53c30
Hieu Tran <tranhieunz@gmail.com>
2020-06-25 23:14:05 +1200

    login silverstripe

--- app/_config.php
+++ app/_config.php
@@ -2,8 +2,13 @@
 
 use SilverStripe\Security\PasswordValidator;
 use SilverStripe\Security\Member;
+// use SilverStripe\View\SSViewer;
 
 // remove PasswordValidator for SilverStripe 5.0
 $validator = PasswordValidator::create();
 // Settings are registered via Injector configuration - see passwords.yml in framework
 Member::set_password_validator($validator);
+
+// SSViewer::set_theme("my-theme");
+// // HtmlEditorConfig::get('cms')->setOption('theme_advanced_styles', 'highlight=highlight;no-border=no-border,break=break');
+
